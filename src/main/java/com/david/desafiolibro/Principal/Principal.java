cfe7947fe6ea588d04b0f859a7dfc8c961aae2d8
dobock13 <davidovni236@gmail.com>
2026-02-28 22:17:03 -0600

    implementacion de todas las funciones

--- src/main/java/com/david/desafiolibro/Principal/Principal.java
+++ src/main/java/com/david/desafiolibro/Principal/Principal.java
@@ -67,28 +67,7 @@ public class Principal {
             }
         }
 
-//        //busqueda por nombre
-//        System.out.println("ingrese el nombre del libro que desea buscar");
-//        var tituloLibro = teclado.nextLine();
-//        json = consumoAPI.obtenerDatos(URL_BASE+"?search="+tituloLibro.replace(" ","+"));
-//        var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
-//        Optional<DatosLibros> libroBuscado = datosBusqueda.resultados().stream()
-//                .filter(l->l.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
-//                .findFirst();
-//        if (libroBuscado.isPresent()){
-//            System.out.println("libro encontrado ");
-//            System.out.println(libroBuscado.get());
-//        }else{
-//            System.out.println("libro no encontrado");
-//        }
-//        //estadisticas
-//        DoubleSummaryStatistics est = datos.resultados().stream()
-//                .filter(d->d.numeroDescargas() > 0)
-//                .collect(Collectors.summarizingDouble(DatosLibros::numeroDescargas));
-//        System.out.println("Cantidad media de descargas: "+est.getAverage());
-//        System.out.println("Cantidad maxima de descargas: "+est.getMax());
-//        System.out.println("Cantidad minima de descargas: "+est.getMin());
-//        System.out.println("Cantidad de muestras evaluadas: "+ est.getCount());
+//
 
     }
 
